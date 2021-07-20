31f26a4ed38b27ee846d1c700e0a67d2a628c819
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2021-07-20 13:47:46 +0530

    changes some style in app.css

--- src/components/Form.js
+++ src/components/Form.js
@@ -38,7 +38,7 @@ const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
         <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>f
+          <option value="uncompleted">Uncompleted</option>
         </select>
       </div>
     </form>
